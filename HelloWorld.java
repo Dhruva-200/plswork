592962a910019116f2a78106a95d35687d2133bd
Dhruva-200 <145975261+Dhruva-200@users.noreply.github.com>
2025-11-26 10:58:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello rrrWorld"); // Prints the message to the console
+        System.out.println("Hello rrrWoreeaeld"); // Prints the message to the console
     }
 
 }
+
